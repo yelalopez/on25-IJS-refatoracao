aeb424debee9d1ae06bc47cbfb31d3b77345d045
yelalopez <yelitzalopezduque@gmail.com>
2023-08-19 15:15:02 -0300

    refatoracao

--- atividade em aula/jogo.js
+++ atividade em aula/jogo.js
@@ -1,69 +1,59 @@
 function iniciaJogo(){
+  let url = window.location.search;
+  let nivelJogo = url.replace("?", "");
+  let tempoSegundo = 0;
 
-var url = window.location.search;
-var nivel_jogo = url.replace("?", "");
+  if(nivelJogo == 1){
+      tempoSegundo = 120;
+  }else if(nivelJogo == 2){
+    tempoSegundo = 60;
+  }else{
+    tempoSegundo = 30;
+  }
 
-var tempo_segundo = 0;
-if(nivel_jogo == 1){//1 facil -> 120 segs
-    tempo_segundo = 120
-}
+  document.getElementById('cronometro').innerHTML = tempoSegundo;
+  let qtdeBaloes = 80;
+  
+  cria_baloes(qtdeBaloes);
 
-if(nivel_jogo == 2){//2 facil -> 60 segs
-    tempo_segundo = 60
-}
+  document.getElementById('baloes_inteiros').innerHTML = qtdeBaloes;
+  document.getElementById('baloes_estourados').innerHTML = 0;
 
-if(nivel_jogo == 3){//3 dificil -> 30 segs
-    tempo_segundo = 30
-}
-
-document.getElementById('cronometro').innerHTML = tempo_segundo;//inserindo segundos no span
-var qtde_baloes = 80;
-//quantidade de baloes
-
-
-cria_baloes(qtde_baloes);
-
-// imprimir qtde de baloes inteiros
-
-document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
-document.getElementById('baloes_estourados').innerHTML = 0;
-
-contagem_tempo(tempo_segundo + 1)
+  contagemTempo (tempoSegundo + 1)
 }
 
 
-function contagem_tempo(segundos){
+function contagemTempo (segundos){
   segundos = segundos - 1;
 
   if (segundos == -1) {
-    clearTimeout(timerId); // para a execucao da funcao settimeout
-    game_over();
+    clearTimeout(timerId); 
+    gameOver();
     return false;
   }
   document.getElementById('cronometro').innerHTML = segundos;
 
-  timerId = setTimeout("contagem_tempo("+segundos+")",1000);
+  timerId = setTimeout("contagemTempo ("+segundos+")",1000);
 }
 
-function game_over(){
+function gameOver(){
    remove_eventos_baloes();
   alert('AAAAAAAAh Acabou o tempo, voce nao conseguiu!')
 }
 
 function remove_eventos_baloes() {
-    var i = 1; //contado para recuperar balões por id
+    let i = 1;
 
-    //percorre o lementos de acordo com o id e só irá sair do laço quando não houver correspondência com elemento
+  o
     while(document.getElementById('b'+i)) {
-        //retira o evento onclick do elemnto
         document.getElementById('b'+i).onclick = '';
-        i++; //faz a iteração da variávei i
+        i++; 
     }
 }
 
-function cria_baloes(qtde_baloes){
-for(var i = 1; i<= qtde_baloes; i++){
-    var balao = document.createElement("img");
+function cria_baloes(qtdeBaloes){
+for(let i = 1; i<= qtdeBaloes; i++){
+    let balao = document.createElement("img");
     balao.src = "imagens/balao_azul_pequeno.png";
 	balao.style.margin = '10px';
   balao.id = 'b'+i;
@@ -74,15 +64,15 @@ for(var i = 1; i<= qtde_baloes; i++){
 }
 
 function estourar(e){
-  var id_balao = e.id;
+  let id_balao = e.id;
   document.getElementById(id_balao).setAttribute("onclick","");
 document.getElementById(id_balao).src = "imagens/balao_azul_pequeno_estourando.png";
 pontuacao(-1);
 }
 
 function pontuacao(acao){
-var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
-var baloes_estourados = document.getElementById('baloes_estourados').innerHTML;
+let baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
+let baloes_estourados = document.getElementById('baloes_estourados').innerHTML;
 
 baloes_inteiros = parseInt(baloes_inteiros);
 baloes_estourados = parseInt(baloes_estourados);
